4bdaa97b8a341927cdfaf10bddb9f179af659e98
signorazai <zairajuliaa@gmail.com>
2024-08-26 13:30:20 +0800

    Analytics Updates from MAIN copied

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\MessageController;
 use App\Http\Controllers\MessageTemplateController;
 use App\Http\Controllers\SubAdminController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ImportController;
 
 // Authentication Routes
 Route::get('/', [AuthController::class, 'index']);
@@ -21,22 +20,21 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
     // Messages
-    Route::get('/admin/messages', [AdminController::class, 'messages'])->name('admin.messages');
+    Route::get('/admin/messages', [MessageController::class, 'showMessagesForm'])->name('admin.messages');
     Route::post('/admin/broadcast', [MessageController::class, 'broadcastToRecipients'])->name('admin.broadcastToRecipients');
     Route::post('/admin/review-message', [MessageController::class, 'reviewMessage'])->name('admin.reviewMessage');
     Route::post('/admin/send-messages', [MessageController::class, 'sendBulkMessages'])->name('admin.send-messages');
-    Route::post('/admin/broadcast-employees', [MessageController::class, 'broadcastToEmployees'])->name('admin.broadcastToEmployees');
+
     // Message Logs
     Route::get('/admin/message-logs', [MessageController::class, 'getMessageLogs'])->name('admin.messageLogs');
 
     // Message Templates CRUD
-    Route::get('/admin/app-management/message-templates', [MessageTemplateController::class, 'index'])->name('message_templates.index'); // List templates
+    Route::get('/admin/app-management/message-templates', [MessageTemplateController::class, 'index'])->name('message_templates.index');
     Route::get('/admin/app-management/message-templates/create', [MessageTemplateController::class, 'create'])->name('message_templates.create');
     Route::post('/admin/app-management/message-templates', [MessageTemplateController::class, 'store'])->name('message_templates.store');
     Route::get('/admin/app-management/message-templates/{id}/edit', [MessageTemplateController::class, 'edit'])->name('message_templates.edit');
     Route::put('/admin/app-management/message-templates/{id}', [MessageTemplateController::class, 'update'])->name('message_templates.update');
     Route::delete('/admin/app-management/message-templates/{id}', [MessageTemplateController::class, 'destroy'])->name('message_templates.destroy');
-
 
     // Analytics
     Route::get('/admin/analytics', [AdminController::class, 'analytics'])->name('admin.analytics');
@@ -70,6 +68,5 @@ Route::get('/api/filters/college/{collegeId}/programs', [FilterController::class
 Route::get('/api/filters/types/{campusId}/{officeId}/{statusId?}', [FilterController::class, 'getTypesByOffice']);
 Route::get('/api/contacts', [FilterController::class, 'getContacts']);
 Route::get('/api/recipients/count', [MessageController::class, 'getRecipientCount']);
-
-// Import
-Route::post('/import', [ImportController::class, 'importData'])->name('import.data');
+Route::get('/api/progress/{logId}', [MessageController::class, 'getProgress']);
+Route::get('/api/analytics', [MessageController::class, 'getAnalyticsData'])->name('api.analytics');
