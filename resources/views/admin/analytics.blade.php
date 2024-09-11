77c9175c46bea7f908c8d576f9b5d964d5006524
joshdrey33 <joshuaandreybanuelos@gmail.com>
2024-09-11 08:36:52 +0800

    copy analytics layout from branch

--- resources/views/admin/analytics.blade.php
+++ resources/views/admin/analytics.blade.php
@@ -52,8 +52,8 @@
                     <p class="text-2xl font-semibold" id="remaining-balance">{{ $balance }}</p>
                 </div>
             </div>
-            Recipient Type Filters
-            <div class="col-span-1">
+            {{-- Recipient Type Filters --}}
+            <div class="col-span-1 pt-4">
                 <label for="campus" class="block text-sm font-medium text-gray-700">Select Campus:</label>
                 <select id="campus"
                     class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
